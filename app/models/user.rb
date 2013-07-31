526c021d5609a32858b7e360f922566d0e19b210
Mentessi <mentessi@gmail.com>
2013-07-31 08:38:16 +0100

    added test for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
    validates :profile_name, presence: true,
                             uniqueness: true,
                             format: {
-                              with: /a-zA-Z0-9_-/,
+                              with: /^[a-zA-Z0-9_-]+$/,
                               message: "Must be formatted correctly."
                             }
 
@@ -22,4 +22,5 @@ class User < ActiveRecord::Base
   def full_name
   	first_name + " " + last_name
   end
+
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -28,12 +28,20 @@ class UserTest < ActiveSupport::TestCase
 	end
 
 	test "a user should have profile name without spaces" do 
-		user = User.new
+		user = User.new(first_name: "Michael", last_name: "Mentessi", email: "mentessi@gmail.com")
+		user.password = user.password_confirmation = "passwords99"
 		user.profile_name = "My profile with spaces"
 
 		assert !user.save
 		assert !user.errors[:profile_name].empty?
 		assert user.errors[:profile_name].include?("Must be formatted correctly.")
+	end
+
+	test "that a correctly formatted profile name works" do 
+		user = User.new(first_name: "Michael", last_name: "Mentessi", email: "mentessi@gmail.com")
+		user.password = user.password_confirmation = "passwords99"
+		user.profile_name = "michael_mentessi1"
+		assert user.valid?
 	end
 
 end
